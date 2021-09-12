f8b09cde1da71f21b71b38e01971f773896433e2
Gustave Granroth <gus.gran@gmail.com>
2021-09-12 11:15:50 -0700

    Throw in a demo ground plane and skybox.

--- CitySim/CitySim/Program.cs
+++ CitySim/CitySim/Program.cs
@@ -48,10 +48,36 @@ namespace CitySim
             var lightnode = scnMgr.getRootSceneNode().createChildSceneNode();
             lightnode.setPosition(0f, 10f, 15f);
             lightnode.attachObject(light);
+            Camera cam = CreateCamera(scnMgr);
 
+            var vp = getRenderWindow().addViewport(cam);
+            vp.setBackgroundColour(new ColourValue(.3f, .3f, .3f));
+
+            var ent = scnMgr.createEntity(@"Sinbad.mesh");
+
+            //TerrainGlobalOptions.getSingleton().
+            //TerrainGroup(scnMgr, Terrain.Alignment.ALIGN_X_Z)
+            Plane plain = new Plane(new Vector3(0, 1, 0), 0);
+            var plane = MeshManager.getSingleton().createPlane("GroundPlane", ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME, plain,
+                1400, 1400, 40, 40, true, 1, 4, 4, new Vector3(0, 0, 1));
+
+            Entity groundEntity = scnMgr.createEntity("GroundObject", "GroundPlane");
+            groundEntity.setCastShadows(false);
+            groundEntity.setMaterialName("Examples/Rockwall");
+
+            scnMgr.setSkyBox(true, "Examples/SpaceSkyBox");
+
+            var node = scnMgr.getRootSceneNode().createChildSceneNode();
+            node.attachObject(ent);
+            node.attachObject(groundEntity);
+        }
+
+        private Camera CreateCamera(SceneManager scnMgr)
+        {
             var cam = scnMgr.createCamera("myCam");
             cam.setAutoAspectRatio(true);
             cam.setNearClipDistance(5);
+            cam.setFarClipDistance(0);
             var camnode = scnMgr.getRootSceneNode().createChildSceneNode();
             camnode.attachObject(cam);
 
@@ -59,14 +85,7 @@ namespace CitySim
             camman.setStyle(CameraStyle.CS_ORBIT);
             camman.setYawPitchDist(new Radian(0), new Radian(0.3f), 15f);
             addInputListener(camman);
-
-            var vp = getRenderWindow().addViewport(cam);
-            vp.setBackgroundColour(new ColourValue(.3f, .3f, .3f));
-
-            ResourceGroupManager.getSingleton().addResourceLocation("Media", "FileSystem", ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME, recursive: true);
-            var ent = scnMgr.createEntity(@"Sinbad.mesh");
-            var node = scnMgr.getRootSceneNode().createChildSceneNode();
-            node.attachObject(ent);
+            return cam;
         }
 
         static void Main()
